0662a9350e88d22aec1622af82f8cf0652393d58
Issam Benelgada <issambenelgada@gmail.com>
2024-07-31 18:05:54 +0200

    First commit of code.

---
+++ ConsoleApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    public class NotificationIcon
+    {
+        static private System.Windows.Forms.NotifyIcon notifyIcon1;
+        static private System.Windows.Forms.ContextMenu contextMenu1;
+        static private System.Windows.Forms.MenuItem menuItem1;
+        static private System.ComponentModel.IContainer components;
+
+        public static void StartNotificationIcon()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenu1 = new System.Windows.Forms.ContextMenu();
+            menuItem1 = new System.Windows.Forms.MenuItem();
+
+            // Initialize contextMenu1
+            contextMenu1.MenuItems.AddRange(
+                        new System.Windows.Forms.MenuItem[] { menuItem1 });
+
+            // Initialize menuItem1
+            menuItem1.Index = 0;
+            menuItem1.Text = "E&xit";
+            menuItem1.Click += new System.EventHandler(menuItem1_Click);
+
+            // Create the NotifyIcon.
+            notifyIcon1 = new System.Windows.Forms.NotifyIcon(components);
+
+            // The Icon property sets the icon that will appear
+            // in the systray for this application.
+            notifyIcon1.Icon = new System.Drawing.Icon("appicon.ico");
+
+            // The ContextMenu property sets the menu that will
+            // appear when the systray icon is right clicked.
+            notifyIcon1.ContextMenu = contextMenu1;
+
+            // The Text property sets the text that will be displayed,
+            // in a tooltip, when the mouse hovers over the systray icon.
+            notifyIcon1.Text = "Form1 (NotifyIcon example)";
+            notifyIcon1.Visible = true;
+
+            // Handle the DoubleClick event to activate the form.
+            notifyIcon1.DoubleClick += new System.EventHandler(menuItem1_Click);
+        }
+
+        static private void menuItem1_Click(object Sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static Process child;
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Application.ApplicationExit += Application_ApplicationExit;
+                child = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = ".\\ConsoleApp1.exe",
+                        Arguments = "arg",
+                        UseShellExecute = false,
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                    }
+                };
+                child.Start();
+                child.StandardInput.WriteLine("Hello world!\n");
+            }
+            else
+            {
+                string s;
+                int a;
+                FileStream logstream = File.OpenWrite(".\\childLog.txt");
+                while (true)
+                {
+                    if((a = Console.OpenStandardInput().ReadByte()) != -1)
+                    {
+                        logstream.WriteByte((byte)a);
+                        logstream.Flush();
+                    }
+                    Thread.Sleep(10);
+                }
+                if(a == 'H')
+                {
+                    NotificationIcon.StartNotificationIcon();
+                }
+            }
+
+            Application.Run();
+        }
+
+        private static void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            if (child != null)
+            {
+                child.Kill();
+            }
+        }
+    }
+}
