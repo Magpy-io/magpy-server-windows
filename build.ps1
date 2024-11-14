513c010eb3d37e9b1e5d4ded758f4485b85825e1
Issam Benelgada <issambenelgada@gmail.com>
2024-11-14 13:23:18 +0100

    Incremented version to 0.6.4.

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-vpk pack -u Magpy -v 0.6.3 -p .\MagpyServerWindows\bin\Release --icon .\MagpyServerWindows\appIcon.ico --packTitle Magpy --packAuthors "Magpy Team." -e Magpy.exe
+vpk pack -u Magpy -v 0.6.4 -p .\MagpyServerWindows\bin\Release --icon .\MagpyServerWindows\appIcon.ico --packTitle Magpy --packAuthors "Magpy Team." -e Magpy.exe
