7706684fbe795a60d09a425020f4269e4b2d2b48
Issam Benelgada <issambenelgada@gmail.com>
2024-11-22 21:34:01 +0100

    Added server periodic updates every 12 hours.

---
+++ MagpyServerWindows/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MagpyServerWindows
+{
+    public class Utils
+    {
+        public static void SchedulePeriodicTask(Func<Task> action, int seconds, CancellationToken token = default)
+        {
+            if (action == null)
+                return;
+            Task.Run(async () => {
+                while (!token.IsCancellationRequested)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(seconds), token);
+                    await action();
+                }
+            }, token);
+        }
+
+        public static void ScheduleTask(Func<Task> action, int seconds, CancellationToken token = default)
+        {
+            if (action == null)
+                return;
+            Task.Run(async () => {
+                if (!token.IsCancellationRequested)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(seconds), token);
+                    await action();
+                }
+            }, token);
+        }
+    }
+}
